14812e7ddf56f7d6a7b3c9e54fb6dcdb5907615d
ananddonepudi <76725976+ananddonepudi@users.noreply.github.com>
2025-12-31 11:51:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello from EC2 via GitHub Actions updated!"
+def hello():
+    return "Hello from Python Docker GitHub Actions!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=5000)
