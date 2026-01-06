07a937df1d1d2be9ed7b7b57ec94d3cb3b9ffe6b
ananddonepudi <76725976+ananddonepudi@users.noreply.github.com>
2026-01-06 16:11:19 +0530

    Update app.py Jan06

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python Docker GitHub Actions!"
+    return "Hello from Python Docker GitHub Actions Jan06!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
